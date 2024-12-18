9309eeb61630a72973127aaa57b7849c178dfe5f
Jafar Safdari <jafar.safdari@winlas.se>
2024-12-18 08:36:18 +0100

    feat: board and tits tests

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,34 @@
+﻿namespace TicTacToe
+{
+    public class Board
+    {
+        private const int Size = 3;
+        private char[,] _cells;
+
+        public Board()
+        {
+            _cells = new char[Size, Size];
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    _cells[i, j] = ' ';
+                }
+            }
+        }
+
+        public char[,] GetCells()
+        {
+            return _cells;
+        }
+
+        public bool PlaceMarker(int x, int y, char marker)
+        {
+            if (_cells[x, y] != ' ')
+                return false;
+
+            _cells[x, y] = marker;
+            return true;
+        }
+    }
+}
---
+++ TicTacToeTest/BoardTests.cs
@@ -0,0 +1,31 @@
+﻿using TicTacToe;
+
+namespace TicTacToeTest;
+
+public class BoardTests
+{
+    [Fact]
+    public void Board_Should_Be_Initialized_With_Empty_Values()
+    {
+        var board = new Board();
+        var cells = board.GetCells();
+
+        var flatCells = cells.Cast<char>();
+
+        Assert.All(flatCells, cell => Assert.Equal(' ', cell));
+    }
+
+    [Fact]
+    public void PlaceMarker_Should_Update_Cell()
+    {
+        var board = new Board();
+        var marker = 'X';
+
+        // Försök att placera markören i den första cellen
+        var result = board.PlaceMarker(0, 0, marker);
+
+        // Testa att det lyckades och cellen nu har markerat värde
+        Assert.True(result);
+        Assert.Equal(marker, board.GetCells()[0, 0]);
+    }
+}
